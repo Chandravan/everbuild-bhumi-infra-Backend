13b33aafe4aafbb0cb9431fced7885e6d6a5f507
chandravan <chandravankumar5145@gmail.com>
2026-02-14 00:19:23 +0530

    debug

--- src/config/loadRatesFromExcel.js
+++ src/config/loadRatesFromExcel.js
@@ -1,22 +1,33 @@
 import xlsx from "xlsx";
 import path from "path";
 import Route from "../models/Route.js";
+import mongoose from "mongoose";
 
-const filePath = path.resolve("data/rate.xlsx");
+const filePath = path.resolve(process.cwd(), "data/rate.xlsx")
 
-// FIXED FROM CITY
-const FROM_CITY = "PATNA";
+
 
 export const loadDestinationRatesToDB = async () => {
   try {
+    //1
+    console.log(" DB Status:", mongoose.connection.readyState); 
+    console.log(" File Path:", filePath);
+
     const workbook = xlsx.readFile(filePath);
     const sheetName = workbook.SheetNames[0];
     const sheet = workbook.Sheets[sheetName];
 
     const rows = xlsx.utils.sheet_to_json(sheet);
+    //2
+    console.log(` Total rows found in Excel: ${rows.length}`);
+    if (rows.length > 0) {
+      console.log("🔍 Sample of first row:", JSON.stringify(rows[0]));
+    }
+
+    
 
     let successCount = 0;
-
+    let skipCount = 0;
     for (const row of rows) {
       const city = row["City"];
 
@@ -26,7 +37,10 @@ export const loadDestinationRatesToDB = async () => {
 
       const rate = row[rateKey];
 
-      if (!city || isNaN(rate)) continue;
+      if (!city || isNaN(rate)){
+        skipCount++;
+        continue;
+      }
 
       const toCity = String(city).trim().toUpperCase();
 
@@ -38,11 +52,14 @@ export const loadDestinationRatesToDB = async () => {
 
       successCount++;
     }
+    //2
+    console.log(`SUCCESS: ${successCount} routes updated.`);
+    console.log(` SKIPPED: ${skipCount} rows (missing city or invalid rate).`);
 
     console.log(
-      `✅ ${successCount} routes saved/updated from Excel (${sheetName})`
+      `${successCount} routes saved/updated from Excel (${sheetName})`
     );
   } catch (error) {
-    console.error("❌ Excel → DB save failed:", error.message);
+    console.error("Excel → DB save failed:", error.message);
   }
 };
